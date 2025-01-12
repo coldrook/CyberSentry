7b0da3e1d8a789db3ee489a887bc2dc7bd4b5cfc
coldrook <8768561+coldrook@users.noreply.github.com>
2025-01-12 17:02:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -397,7 +397,7 @@ fi
 echo "配置 Cowrie 服务..."
 PYTHON_VERSION=$(get_python_version)
 # 查找 site-packages 目录
-SITE_PACKAGES_DIR=$(find "$COWRIE_INSTALL_DIR/cowrie-env/lib/" -maxdepth 3 -type d -name "site-packages" -print -quit)
+SITE_PACKAGES_DIR=$(find "$COWRIE_INSTALL_DIR/cowrie/cowrie-env/lib/" -maxdepth 3 -type d -name "site-packages" -print -quit)
 if [ -z "$SITE_PACKAGES_DIR" ]; then
     echo "错误：无法找到 site-packages 目录"
     exit 1
