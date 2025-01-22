6114da7ff7f07caa08b406940b2d9766fdac23ab
coldrook <8768561+coldrook@users.noreply.github.com>
2025-01-22 22:55:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -55,14 +55,6 @@ apt install -y logrotate || {
     exit 1
 }
 
-# 函数定义
-check_command() {
-    command -v "$1" >/dev/null 2>&1 || {
-        echo "错误：未找到命 $1"
-        exit 1
-    }
-}
-
 # 检查 ufw 是否已安装
 if check_command ufw; then
     echo "ufw 已安装"
@@ -82,6 +74,14 @@ else
         echo "跳过安装 ufw"
     fi
 fi
+
+# 函数定义
+check_command() {
+    command -v "$1" >/dev/null 2>&1 || {
+        echo "错误：未找到命 $1"
+        exit 1
+    }
+}
 
 backup_config() {
     local config_file="$1"
